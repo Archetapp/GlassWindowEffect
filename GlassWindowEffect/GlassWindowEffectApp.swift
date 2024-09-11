1fb5459d6ddc7c106a29a6d9bb9a6af2592bc7dc
Jared Davidson (Archetapp) <archetapp@gmail.com>
2024-09-11 16:43:36 -0600

    Initial commit

---
+++ GlassWindowEffect/GlassWindowEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassWindowEffectApp.swift
+//  GlassWindowEffect
+//
+//  Created by Jared Davidson on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GlassWindowEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
